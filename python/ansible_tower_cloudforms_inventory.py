82798573af0ae41884b395e54a6ff90b8fa5dfbd
Geert Jansen <geertj@gmail.com>
2015-11-18 10:52:31 -0500

    pep8 compliant indentation

--- python/ansible_tower_cloudforms_inventory.py
+++ python/ansible_tower_cloudforms_inventory.py
@@ -41,14 +41,14 @@ class CloudFormsInventory(object):
         self.read_settings()
         self.parse_cli_args()
 
-	# Get Hosts
-	if self.args.list:
-		self.get_hosts()
+        # Get Hosts
+        if self.args.list:
+            self.get_hosts()
 
-	# This doesn't exist yet and needs to be added
+        # This doesn't exist yet and needs to be added
         if self.args.host:
-		data2 = { }
-		print json.dumps(data2, indent=2)
+            data2 = { }
+            print json.dumps(data2, indent=2)
 
     def parse_cli_args(self):
         ''' Command line argument processing '''
@@ -56,7 +56,7 @@ class CloudFormsInventory(object):
         parser = argparse.ArgumentParser(description='Produce an Ansible Inventory file based on CloudForms')
         parser.add_argument('--list', action='store_true', default=False,
                            help='List instances (default: False)')
-	parser.add_argument('--host', action='store',
+        parser.add_argument('--host', action='store',
                            help='Get all the variables about a specific instance')
         self.args = parser.parse_args()
 
@@ -68,56 +68,56 @@ class CloudFormsInventory(object):
         cloudforms_ini_path = "/opt/rh/cloudforms.ini"
         config.read(cloudforms_ini_path)
 
-	# Version
-	if config.has_option('cloudforms', 'version'):
-		self.cloudforms_version = config.get('cloudforms', 'version')
-	else:
-		self.cloudforms_version = "none"
+        # Version
+        if config.has_option('cloudforms', 'version'):
+            self.cloudforms_version = config.get('cloudforms', 'version')
+        else:
+            self.cloudforms_version = "none"
 
-	# CloudForms Endpoint
-	if config.has_option('cloudforms', 'hostname'):
-		self.cloudforms_hostname = config.get('cloudforms', 'hostname')
-	else:
-		self.cloudforms_hostname = None
+        # CloudForms Endpoint
+        if config.has_option('cloudforms', 'hostname'):
+            self.cloudforms_hostname = config.get('cloudforms', 'hostname')
+        else:
+            self.cloudforms_hostname = None
 
-	# CloudForms Username
+        # CloudForms Username
         if config.has_option('cloudforms', 'username'):
-                self.cloudforms_username = config.get('cloudforms', 'username')
+            self.cloudforms_username = config.get('cloudforms', 'username')
         else:
-                self.cloudforms_username = "none"
+            self.cloudforms_username = "none"
 
-	# CloudForms Password
+        # CloudForms Password
         if config.has_option('cloudforms', 'password'):
-                self.cloudforms_password = config.get('cloudforms', 'password')
+            self.cloudforms_password = config.get('cloudforms', 'password')
         else:
-                self.cloudforms_password = "none"
-		
+            self.cloudforms_password = "none"
+
     def get_hosts(self):
-	''' Gets host from CloudForms '''	
-	r = requests.get("https://" + self.cloudforms_hostname + "/api/vms?expand=resources&attributes=name,power_state", auth=(self.cloudforms_username,self.cloudforms_password), verify=False)
+        ''' Gets host from CloudForms '''
+        r = requests.get("https://" + self.cloudforms_hostname + "/api/vms?expand=resources&attributes=name,power_state", auth=(self.cloudforms_username,self.cloudforms_password), verify=False)
 
-	obj = r.json()
+        obj = r.json()
 
-	#Remove objects that don't matter	
-	del obj["count"]
-	del obj["subcount"]
-	del obj["name"]
+        #Remove objects that don't matter
+        del obj["count"]
+        del obj["subcount"]
+        del obj["name"]
 
-	#Create a new list to grab VMs with power_state on to add to a new list	
-	#I'm sure there is a cleaner way to do this
-	newlist = []
-	getnext = False
-	for x in obj.items():
-		for y in x[1]:
-			for z in y.items():
-				if getnext == True:
-					newlist.append(z[1])
-					getnext = False
-				if ( z[0] == "power_state" and z[1] == "on" ):
-					getnext = True
-	newdict = {'hosts': newlist}
-	newdict2 = {'Dynamic_CloudForms': newdict}
-	print json.dumps(newdict2, indent=2)
+        #Create a new list to grab VMs with power_state on to add to a new list
+        #I'm sure there is a cleaner way to do this
+        newlist = []
+        getnext = False
+        for x in obj.items():
+            for y in x[1]:
+                for z in y.items():
+                    if getnext == True:
+                        newlist.append(z[1])
+                        getnext = False
+                    if ( z[0] == "power_state" and z[1] == "on" ):
+                        getnext = True
+        newdict = {'hosts': newlist}
+        newdict2 = {'Dynamic_CloudForms': newdict}
+        print json.dumps(newdict2, indent=2)
 
 # Run the script
 CloudFormsInventory()
